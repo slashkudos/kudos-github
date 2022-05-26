a85ae90592610be62a8e39572c2663016d2730d0
Philip Gai <PhilipMGai@gmail.com>
2022-05-26 19:34:27 +0000

    Init github webhooks api and lambda function

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "kudosgithubwebhookhandler": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "github": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
