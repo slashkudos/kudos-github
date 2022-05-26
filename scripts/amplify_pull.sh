8b00acd47fa4dde1244fd1e4590240e3448eb8a6
Philip Gai <PhilipMGai@gmail.com>
2022-05-26 19:40:36 +0000

    The github app doesn't use graphql
    So don't worry about hiding the graphql key output that won't be there :)

--- scripts/amplify_pull.sh
+++ scripts/amplify_pull.sh
@@ -35,7 +35,7 @@ if [ -z "$(amplify env get --name $amplify_env | grep 'No environment found')" ]
     rm -rf $ROOT_DIR/amplify-temp
 
     # GraphQL transformer version 2 outputs GraphQL API Key even with "CreateAPIKey": 0
-    amplify status -v | grep -v "GraphQL API KEY"
+    amplify status -v
 
     status=$?
     exit $status
