facc64e62c1010474e8e1072e6a8e430fd154f9d
Philip Gai <philip@slashkudos.com>
2022-05-27 15:54:55 -0500

    Fix lambda (#10)
    * Mock the function in the workflows
    
    * Update path
    
    * Update module type
    
    * Move from lib to src because of amplify function path hardcode
    
    * Update asset path
    
    * Fix export
    
    * Add amplify status -v to ci

--- scripts/amplify_prebuild.sh
+++ scripts/amplify_prebuild.sh
@@ -1,29 +1,26 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+ROOT_DIR=$SCRIPT_DIR/..
+FUNCTION_DIR=amplify/backend/function/githubwebhookshandler
+
+cd $ROOT_DIR
 echo "Executing npm ci in $(pwd)"
 npm ci
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-ROOT_DIR=$SCRIPT_DIR/..
-FUNCTION_DIR=amplify/backend/function/githubwebhookshandler/src
-
-cd $ROOT_DIR/$FUNCTION_DIR
+cd $ROOT_DIR/$FUNCTION_DIR/lib
 echo "Executing npm ci, lint and build in $(pwd)"
 npm ci
 npm run build
 cd -
 
 echo "Copying lib/.npmrc into src folder"
-cp $ROOT_DIR/$FUNCTION_DIR/.npmrc $ROOT_DIR/$FUNCTION_DIR/lib
+cp $ROOT_DIR/$FUNCTION_DIR/lib/.npmrc $ROOT_DIR/$FUNCTION_DIR/src
 
-cd $ROOT_DIR/$FUNCTION_DIR/lib
+cd $ROOT_DIR/$FUNCTION_DIR/src
 echo "Executing npm ci --production in $(pwd)"
 npm ci --production
 cd -
-
-if command -v amplify &>/dev/null; then
-  amplify status -v
-fi
 
 echo "Done"
